1530734a9cba55576c18cf19398c821e3ea7fff4
potterbl <zeexample42@gmail.com>
2025-04-02 13:17:54 +0300

    fix build

--- src/pages/seasonsCreate.jsx
+++ src/pages/seasonsCreate.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useState} from 'react';
 import "../styles/usersCreate.css";
 import EditHead from "../components/editHead";
 import {useNavigate} from "react-router-dom";
